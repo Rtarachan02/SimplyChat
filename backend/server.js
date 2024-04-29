8c5fb4a2eae78a12afb54c025cfbfba7bbb552f0
Tarachan Rana <103596256+Rtarachan02@users.noreply.github.com>
2024-04-29 13:17:54 +0530

    Some Updates

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -9,15 +8,17 @@ import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve();
 dotenv.config();
 app.use(express.json());//to parse the incoming request from json payloads(from req.body)
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);//access the user route
-// app.get("/",(req,res) => {
-//   res.send("Hello World!Tarachand Rana");
-// });
+app.use(express.static(path.join(__dirname,"/frontend/dist")))//express.static to serve as a static files
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT,() => {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`);//ctrl x the readd after curly brace
